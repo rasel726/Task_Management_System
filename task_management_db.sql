e6af57a8e12e764c4d3733754f259c133ae72d3f
rasel726 <md.rasel2726@gmail.com>
2025-02-05 22:07:03 +0600

    Updated repository URL

--- task_management_db.sql
+++ task_management_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 06, 2024 at 10:40 AM
+-- Generation Time: Feb 05, 2025 at 04:28 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -56,7 +56,15 @@ INSERT INTO `notifications` (`id`, `message`, `recipient`, `type`, `date`, `is_r
 (13, '\'Schedule team meeting\' has been assigned to you. Please review and start working on it', 2, 'New Task Assigned', '2024-09-06', 0),
 (14, '\'Prepare budget for Q4\' has been assigned to you. Please review and start working on it', 7, 'New Task Assigned', '2024-09-06', 0),
 (15, '\'Write blog post on industry trend\' has been assigned to you. Please review and start working on it', 7, 'New Task Assigned', '2024-09-06', 0),
-(16, '\'Renew software license\' has been assigned to you. Please review and start working on it', 2, 'New Task Assigned', '2024-09-06', 0);
+(16, '\'Renew software license\' has been assigned to you. Please review and start working on it', 2, 'New Task Assigned', '2024-09-06', 0),
+(17, '\'Renew software license\' has been assigned to you. Please review and start working on it', 10, 'New Task Assigned', '2025-01-27', 1),
+(18, '\'Write blog post on industry trend\' has been assigned to you. Please review and start working on it', 8, 'New Task Assigned', '2025-01-28', 0),
+(19, '\'Prepare budget for Q4\' has been assigned to you. Please review and start working on it', 10, 'New Task Assigned', '2025-01-28', 0),
+(20, '\'Schedule team meeting\' has been assigned to you. Please review and start working on it', 11, 'New Task Assigned', '2025-01-28', 0),
+(21, '\'Conduct software testing\' has been assigned to you. Please review and start working on it', 12, 'New Task Assigned', '2025-01-28', 0),
+(22, '\'an\' has been assigned to you. Please review and start working on it', 2, 'New Task Assigned', '2025-01-30', 1),
+(23, '\'new task\' has been assigned to you. Please review and start working on it', 19, 'New Task Assigned', '2025-01-30', 1),
+(24, '\'new task\' has been assigned to you. Please review and start working on it', 7, 'New Task Assigned', '2025-01-30', 1);
 
 -- --------------------------------------------------------
 
@@ -79,23 +87,31 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `title`, `description`, `assigned_to`, `due_date`, `status`, `created_at`) VALUES
-(1, 'Task 1', 'Task Description', 7, NULL, 'completed', '2024-08-29 16:47:37'),
-(4, 'Monthly Financial Report Preparation', 'Prepare and review the monthly financial report, including profit and loss statements, balance sheets, and cash flow analysis.', 7, '2024-09-01', 'completed', '2024-08-31 10:50:20'),
-(5, 'Customer Feedback Survey Analysis', 'Collect and analyze data from the latest customer feedback survey to identify areas for improvement in customer service.', 7, '2024-09-03', 'in_progress', '2024-08-31 10:50:47'),
-(6, 'Website Maintenance and Update', 'Perform regular maintenance on the company website, update content, and ensure all security patches are applied.', 7, '2024-09-03', 'pending', '2024-08-31 10:51:12'),
-(7, 'Quarterly Inventory Audit', 'Conduct a thorough audit of inventory levels across all warehouses and update the inventory management system accordingly.', 2, '2024-09-03', 'completed', '2024-08-31 10:51:45'),
-(8, 'Employee Training Program Development', 'Develop and implement a new training program focused on enhancing employee skills in project management and teamwork.', 2, '2024-09-01', 'pending', '2024-08-31 10:52:11'),
-(17, 'Prepare monthly sales report', 'Compile and analyze sales data for the previous month', 7, '2024-09-06', 'pending', '2024-09-06 08:01:48'),
-(18, 'Update client database', 'Ensure all client information is current and complete', 7, '2024-09-07', 'pending', '2024-09-06 08:02:27'),
-(19, 'Fix server downtime issue', 'Investigate and resolve the cause of recent server downtimes', 2, '2024-09-07', 'pending', '2024-09-06 08:02:59'),
-(20, 'Plan annual marketing strategy', 'Develop a comprehensive marketing strategy for the next year', 2, '2024-09-04', 'pending', '2024-09-06 08:03:21'),
-(21, 'Onboard new employees', 'Complete HR onboarding tasks for the new hires', 7, '2024-09-07', 'pending', '2024-09-06 08:03:44'),
-(22, 'Design new company website', 'Create wireframes and mockups for the new website design', 2, '2024-09-06', 'pending', '2024-09-06 08:04:20'),
-(23, 'Conduct software testing', 'Run tests on the latest software release to identify bugs', 7, '2024-09-07', 'pending', '2024-09-06 08:04:39'),
-(24, 'Schedule team meeting', 'Organize a meeting to discuss project updates', 2, '2024-09-07', 'pending', '2024-09-06 08:04:57'),
-(25, 'Prepare budget for Q4', 'Create and review the budget for the upcoming quarter', 7, '2024-09-07', 'pending', '2024-09-06 08:05:21'),
-(26, 'Write blog post on industry trend', 'Draft and publish a blog post about current industry trend', 7, '2024-09-07', 'pending', '2024-09-06 08:10:50'),
-(27, 'Renew software license', 'Ensure all software licenses are renewed and up to date', 2, '2024-09-06', 'pending', '2024-09-06 08:11:28');
+(1, 'Quarterly Inventory Audit', 'Conduct a thorough audit of inventory levels across all warehouses and update the inventory management system accordingly.', 7, '2025-01-29', 'completed', '2024-08-29 16:47:37'),
+(4, 'Monthly Financial Report Preparation', 'Prepare and review the monthly financial report, including profit and loss statements, balance sheets, and cash flow analysis.', 7, '2025-01-30', 'completed', '2024-08-31 10:50:20'),
+(5, 'Customer Feedback Survey Analysis', 'Collect and analyze data from the latest customer feedback survey to identify areas for improvement in customer service.', 7, '2025-02-13', 'in_progress', '2024-08-31 10:50:47'),
+(6, 'Website Maintenance and Update', 'Perform regular maintenance on the company website, update content, and ensure all security patches are applied.', 7, '2025-02-14', 'pending', '2024-08-31 10:51:12'),
+(7, 'Quarterly Inventory Audit', 'Conduct a thorough audit of inventory levels across all warehouses and update the inventory management system accordingly.', 2, '2025-01-31', 'completed', '2024-08-31 10:51:45'),
+(8, 'Employee Training Program Development', 'Develop and implement a new training program focused on enhancing employee skills in project management and teamwork.', 2, '2025-02-05', 'pending', '2024-08-31 10:52:11'),
+(17, 'Prepare monthly sales report', 'Compile and analyze sales data for the previous month', 7, '2025-02-11', 'pending', '2024-09-06 08:01:48'),
+(18, 'Update client database', 'Ensure all client information is current and complete', 7, '2025-02-17', 'pending', '2024-09-06 08:02:27'),
+(19, 'Fix server downtime issue', 'Investigate and resolve the cause of recent server downtimes', 2, '2025-02-18', 'pending', '2024-09-06 08:02:59'),
+(20, 'Plan annual marketing strategy', 'Develop a comprehensive marketing strategy for the next year', 2, '2025-02-20', 'pending', '2024-09-06 08:03:21'),
+(21, 'Onboard new employees', 'Complete HR onboarding tasks for the new hires', 7, '0000-00-00', 'pending', '2024-09-06 08:03:44'),
+(22, 'Design new company website', 'Create wireframes and mockups for the new website design', 2, '2025-02-15', 'pending', '2024-09-06 08:04:20'),
+(23, 'Conduct software testing', 'Run tests on the latest software release to identify bugs', 7, '2025-02-05', 'pending', '2024-09-06 08:04:39'),
+(24, 'Schedule team meeting', 'Organize a meeting to discuss project updates', 2, '2025-02-12', 'pending', '2024-09-06 08:04:57'),
+(25, 'Prepare budget for Q4', 'Create and review the budget for the upcoming quarter', 7, '2025-02-19', 'pending', '2024-09-06 08:05:21'),
+(26, 'Write blog post on industry trend', 'Draft and publish a blog post about current industry trend', 7, '2025-02-05', 'pending', '2024-09-06 08:10:50'),
+(27, 'Renew software license', 'Ensure all software licenses are renewed and up to date', 2, '2025-02-12', 'pending', '2024-09-06 08:11:28'),
+(28, 'Renew software license', 'Ensure all software licenses are renewed and up to date', 10, '2025-02-03', 'in_progress', '2025-01-27 17:48:22'),
+(29, 'Write blog post on industry trend', 'Draft and publish a blog post about current industry trend', 8, '2025-01-31', 'in_progress', '2025-01-28 17:58:51'),
+(30, 'Prepare budget for Q4', 'Create and review the budget for the upcoming quarter', 10, '2025-02-06', 'pending', '2025-01-28 17:59:16'),
+(31, 'Schedule team meeting', 'Organize a meeting to discuss project updates', 11, '2025-01-24', 'in_progress', '2025-01-28 17:59:38'),
+(32, 'Conduct software testing', 'Run tests on the latest software release to identify bugs', 12, '2025-01-30', 'completed', '2025-01-28 17:59:59'),
+(33, 'an', 'na', 2, '0000-00-00', 'in_progress', '2025-01-30 12:28:51'),
+(34, 'new task', 'na', 19, '2025-01-31', 'in_progress', '2025-01-30 12:38:48'),
+(35, 'new task', 'nanananna', 7, '2025-02-05', 'in_progress', '2025-01-30 12:46:06');
 
 -- --------------------------------------------------------
 
@@ -117,10 +133,18 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `full_name`, `username`, `password`, `role`, `created_at`) VALUES
-(1, 'Oliver', 'admin', '$2y$10$TnyR1Y43m1EIWpb0MiwE8Ocm6rj0F2KojE3PobVfQDo9HYlAHY/7O', 'admin', '2024-08-28 07:10:04'),
-(2, 'Elias A.', 'elias', '$2y$10$8xpI.hVCVd/GKUzcYTxLUO7ICSqlxX5GstSv7WoOYfXuYOO/SZAZ2', 'employee', '2024-08-28 07:10:40'),
-(7, 'John', 'john', '$2y$10$CiV/f.jO5vIsSi0Fp1Xe7ubWG9v8uKfC.VfzQr/sjb5/gypWNdlBW', 'employee', '2024-08-29 17:11:21'),
-(8, 'Oliver', 'oliver', '$2y$10$E9Xx8UCsFcw44lfXxiq/5OJtloW381YJnu5lkn6q6uzIPdL5yH3PO', 'employee', '2024-08-29 17:11:34');
+(1, 'Rasel Sarkar', 'admin', '$2y$10$TnyR1Y43m1EIWpb0MiwE8Ocm6rj0F2KojE3PobVfQDo9HYlAHY/7O', 'admin', '2024-08-28 07:10:04'),
+(2, 'Mahin Kabir', 'mahin', '$2y$10$GqGdJWYvuzzzTwKCHfX2jOzQ8hgzObQHjX2HhJKG7Xu9EdDOEGGfC', 'employee', '2024-08-28 07:10:40'),
+(7, 'Ariful Islam', 'arif', '$2y$10$rWQ8u0Fu.aWm4H7jzsdRpu9VazpKpFL1u.bc15FKHmzHbqnsKn4T6', 'employee', '2024-08-29 17:11:21'),
+(8, 'Shanto Mozumder', 'shanto', '$2y$10$omGPlLdjkO9JG6ZPDAwK3ucPodzZoCN4v/jtRrhPB97xM9PxX5oly', 'employee', '2024-08-29 17:11:34'),
+(10, 'Tamal Rahman', 'tamal', '$2y$10$tNwo8W3VPvzW5IWaPEBNGOGgfBmYBqLKZtoTHZ6z2aASP/eRzCLbm', 'employee', '2025-01-27 17:44:29'),
+(11, 'Rakib Hossain', 'rakib', '$2y$10$BhQuHOkrRVqAJu89HMTZTefS2oaenyNtrx8bNz674avq3XnThQJda', 'employee', '2025-01-28 17:54:33'),
+(12, 'Sohel Rana', 'sohel', '$2y$10$EvvtXyGXBq6uTxJ9Kg3Ml.VBET2PebK4JcC1mhXE5zfykzWEXiZri', 'employee', '2025-01-28 17:55:13'),
+(13, 'Tushar Ahmed', 'tushar', '$2y$10$Rzfgyw/G4yMnGiMnmagwXOs8uq3iRdSR2U8TLhinnX7BmUEpClQGi', 'employee', '2025-01-28 17:55:28'),
+(14, 'Rashedul Islam', 'rashedul', '$2y$10$bAwFBzoyYz0UB3aA2jY9m.v455VwYjKuHqOe9uoF3YMZ.4NEc2QlO', 'employee', '2025-01-28 17:56:08'),
+(15, 'Emon Khan', 'emon', '$2y$10$6SnSvkUFN.JeEEGq5/ow3.bK.bsaxmygd1fH5mXXWCwJCSGSxWDnS', 'employee', '2025-01-28 17:56:30'),
+(17, 'Suman Halder', 'suman', '$2y$10$2fQKb3NI7EaCjSLuz1bTgu2dF/Whn54oTHepMnYWy2SLsvQvD1Qt.', 'employee', '2025-01-28 18:02:41'),
+(19, 'Kabir Hossen', 'kabir', '$2y$10$AQqn8Wo04R3LMX0GIVTpiuEnnxNV4CG88Iw3vtrprJkuXAbn6JxZm', 'employee', '2025-01-30 12:36:32');
 
 --
 -- Indexes for dumped tables
@@ -153,19 +177,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Constraints for dumped tables
